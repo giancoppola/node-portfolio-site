7c19389af8cb749fa86041f8f2f9fafae52f7cce
giancoppola <44214851+giancoppola@users.noreply.github.com>
2024-02-25 23:42:31 +0000

    api in react

--- src/test.tsx
+++ src/test.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
 const ReactDOM = require('react-dom');
+// MongoDB model imports
+import {User} from '../server/models';
 
 class App extends React.Component{
     static Title(){
         return <h1>This is a title</h1>
+        User.Model
+    }
+    static async Users(){
+        let users: Array<Object> = await fetch('/api/users').then((res) => res.json());
+        for (let user in users){
+            <App.User/>
+        }
+    }
+    static User(){
+        return <p>User found</p>
     }
     constructor(props: any){
         super(props);
