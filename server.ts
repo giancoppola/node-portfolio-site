dc395d8ef54cbeb84394dd60d3aed9c3232f686f
Gian <giancoppola@hotmail.co.uk>
2024-03-11 17:44:08 +0000

    minor

--- server.ts
+++ server.ts
@@ -24,7 +24,7 @@ app.get("/", (req: Request, res: Response) => {
 })
 
 // add GET requests for pages
-const pageArr: Array<String> = ['meal-picker']
+const pageArr: Array<String> = ['meal-planner']
 for (let page of pageArr){
     app.get(`/${page}`, (req: Request, res:Response, next: NextFunction) => {
         res.sendFile(__dirname + `/views/${page}.html`)
