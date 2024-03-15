861379792f1ca8e80ade84c8589f87dea505d009
Gian <giancoppola@hotmail.co.uk>
2024-03-15 17:57:33 +0000

    started planner

--- src/meals/_planner.js
+++ src/meals/_planner.js
@@ -3,9 +3,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Planner = void 0;
+exports.Planner = exports.Days = exports.AddButton = void 0;
 var react_1 = __importDefault(require("react"));
+var AddButton = function () {
+    return (react_1.default.createElement("button", { className: "planner-section__add", id: "add-day", onClick: function () { return console.log('add'); } },
+        react_1.default.createElement("img", { src: "../img/icons/material-add-dm-primary.svg", alt: "Add Icon" }),
+        "Add"));
+};
+exports.AddButton = AddButton;
+var Days = function () {
+    return (react_1.default.createElement("div", { className: "days", id: "days" },
+        react_1.default.createElement("ul", { className: "days__list", id: "days-list" })));
+};
+exports.Days = Days;
 var Planner = function () {
-    return (react_1.default.createElement("h2", null, "planner"));
+    return (react_1.default.createElement("section", { className: "narrow-container planner-section", id: "planner" },
+        react_1.default.createElement("h2", null, "planner"),
+        react_1.default.createElement(exports.Days, null),
+        react_1.default.createElement(exports.AddButton, null)));
 };
 exports.Planner = Planner;
--- src/meals/_planner.tsx
+++ src/meals/_planner.tsx
@@ -2,8 +2,32 @@ import React, {useState, useEffect} from 'react';
 import {Outlet} from 'react-router-dom';
 import { iMeal } from '../../server/meals';
 
+export const AddButton = () => {
+    return (
+        <button className="planner-section__add" id="add-day"
+        onClick={() => console.log('add')}>
+            <img src="../img/icons/material-add-dm-primary.svg" alt="Add Icon" />
+            Add
+        </button>
+    )
+}
+
+export const Days = () => {
+    return (
+        <div className="days" id="days">
+            <ul className="days__list" id="days-list">
+
+            </ul>
+        </div>
+    )
+}
+
 export const Planner = () => {
     return (
-        <h2>planner</h2>
+        <section className="narrow-container planner-section" id="planner">
+            <h2>planner</h2>
+            <Days/>
+            <AddButton/>
+        </section>
     )
 }
